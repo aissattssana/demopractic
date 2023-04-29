8b8a798a9cc9e52774dc9c6923127673fc35fade
asya <anoshina050503@gmail.com>
2023-04-29 12:01:11 +0300

    Добавьте файлы проекта.

---
+++ wpfsport/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpfsport
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public SportdbContext sport = new SportdbContext();
+        private void entr_Click(object sender, RoutedEventArgs e)
+        { 
+            
+             
+                if (sport.Users.Any(i => i.UserLogin == login.Text && i.UserPassword == password.Text && i.RoleId == 1))
+                { 
+                        MessageBox.Show("Успешная авторизация клиента");
+                    Tovars tov = new Tovars();
+                    tov.Show();
+                    this.Hide();
+                    }
+                    if (sport.Users.Any(i => i.UserLogin == login.Text && i.UserPassword == password.Text && i.RoleId == 2))
+                    {
+                        MessageBox.Show("Успешная авторизация администратора");
+                    }
+
+                    if (sport.Users.Any(i => i.UserLogin == login.Text && i.UserPassword == password.Text && i.RoleId == 3))
+                    {
+                        MessageBox.Show("Успешная авторизация менеджера");
+                    }
+            
+        }
+    }
+}
---
+++ wpfsport/Tovars.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace wpfsport
+{
+    /// <summary>
+    /// Логика взаимодействия для Tovars.xaml
+    /// </summary>
+    public partial class Tovars : Window
+    {
+        public SportdbContext sport = new SportdbContext();
+        public Tovars()
+        {
+            InitializeComponent();
+            sport = new SportdbContext();
+            //sport.Products.Load();
+            ProductGrid.ItemsSource = sport.Products.ToList();
+
+
+        }
+
+        private void entr_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow m = new MainWindow();
+            m.Show();
+            this.Hide();
+            } 
+
+        public void ProductGrid_Selected(object sender, RoutedEventArgs e) { }
+        public void ProductGrid_SelectionChanged (object sender, SelectionChangedEventArgs e) { }
+        public void ProductGrid_SelectedCellsChanged (object sender, SelectionChangedEventArgs e) { }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ComboBoxItem_Selected(object sender, RoutedEventArgs e)
+        {
+            ProductGrid.ItemsSource = sport.Products.Where(x=>x.ProductDiscountAmount <10 ).ToList();
+        }
+
+        private void ComboBoxItem_Selected_1(object sender, RoutedEventArgs e)
+        {
+
+            ProductGrid.ItemsSource = sport.Products.Where(x => x.ProductDiscountAmount >10 && x.ProductDiscountAmount<15).ToList();
+        }
+
+        private void ComboBoxItem_Selected_2(object sender, RoutedEventArgs e)
+        {
+
+            ProductGrid.ItemsSource = sport.Products.Where(x => x.ProductDiscountAmount > 15 && x.ProductDiscountAmount < 100).ToList();
+        }
+
+        private void ComboBoxItem_Selected_3(object sender, RoutedEventArgs e)
+        {
+
+            ProductGrid.ItemsSource = sport.Products.ToList();
+        }
+    }
+}
